d79d9e30cf2cad971dbeea9a8613036742c0ec1f
Leon Miller-Out <leon@singlebrook.com>
2016-01-25 10:54:07 -0500

    Put crash reproduction code in a single file

--- crash.rb
+++ crash.rb
@@ -1,5 +1,16 @@
-require 'rubygems'
-require 'bundler/setup'
+begin
+  require 'bundler/inline'
+rescue LoadError => e
+  $stderr.puts 'Bundler version 1.10 or later is required. Please update your Bundler'
+  raise e
+end
+
+gemfile(true) do
+  ruby '2.3.0'
+  source 'https://rubygems.org'
+  gem 'activerecord', '4.2.5'
+end
+
 require 'active_record'
 
 { foo: ActiveRecord::Base }.dig(:foo, :bar)
